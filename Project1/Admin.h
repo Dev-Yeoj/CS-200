e29bf86fd705289f09206aca677bd67cdcc83a88
Rockhockey <56411779+Rockhockey@users.noreply.github.com>
2019-11-13 18:49:24 -0500

    Add files via upload

---
+++ Project1/Admin.h
@@ -0,0 +1,16 @@
+//Admin.h
+#include<iostream>
+#include"Account.h"
+#ifndef ADMIN_H
+#define ADMIN_H
+class Admin:public Account{
+	public:
+		Admin();
+		Admin(string, string);
+		void addUser(string, string);
+		void addTeller(string, string);
+		void addAdmin(string, string);
+		void viewLog();
+		void print();
+};
+#endif
