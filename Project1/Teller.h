2c599959cc31d9039e62e6addac7ac09c5c940a0
Rockhockey <56411779+Rockhockey@users.noreply.github.com>
2019-11-13 18:46:18 -0500

    Add files via upload

---
+++ Project1/Teller.h
@@ -0,0 +1,14 @@
+//Teller.h
+#include<iostream>
+#include"Account.h"
+#ifndef TELLER_H
+#define TELLER_H
+class Teller:public Account{
+	public:
+		Teller();
+		Teller(string, string);
+		void addUser(string, string);
+		void openAccount(string, string);
+		void print();
+};
+#endif
