3fac30138df3de614e13112fb7dec942c39d1426
Rockhockey <56411779+Rockhockey@users.noreply.github.com>
2019-11-13 18:40:47 -0500

    Add files via upload

---
+++ Project1/Client.h
@@ -0,0 +1,16 @@
+//Client.h
+#include<iostream>
+#include"Account.h"
+#ifndef CLIENT_H
+#define CLIENT_H
+class Client:public Account{
+	private:
+		double bal;
+	public:
+		Client();
+		double getBal();
+		void deposit(double);
+		void withdraw(double);
+		void print();
+};
+#endif
