b1119299a744d13bd6f8c1f8ef028d4f65f783e8
Marc <56411779+Marc@users.noreply.github.com>
2019-11-13 23:46:35 -0500

    set up project together
    add main file and include all headers

---
+++ Project1/BankingProject.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include"Account.h"
+#include"Admin.h"
+#include"Client.h"
+#include"Teller.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	return 0;
+}
