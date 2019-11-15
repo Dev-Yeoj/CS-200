5c6ab1820e898dbf11592088a6e20cbd817dc2b9
Dev-Yeoj <Dev-Yeoj@users.noreply.github.com>
2019-11-15 14:42:47 -0500

    Restructured and added read and decrypt

--- Project1/BankingProject.cpp
+++ Project1/BankingProject.cpp
@@ -11,51 +11,69 @@ Account* accPTR[100];
 int count=0;
 
 
-int main(int argc, char** argv) {
-	//variable initialization
-	int choice;
-	int flag;
-	string id, pswd;
-	//decrypt and load
+string readAndDecrypt()
+{
+	char key = 'q';
+	//ofstream adminsFile;
+	ifstream tellersFile;
+	ifstream clientsFile;
+	ifstream adminsFile;
+	string dataIn;
+	string decryptedData;
+	string space = " ";
 	
-	//login & menu
-	do{
-		flag=1;
-		cout<<"Please enter account ID and password(to exit program leave blank): ";
-		cin>>id, pswd;
-		for(i=0; i<count;i++){
-			if(accPTR[i].getID()==id && accPTR[i].getPass()==pswd){
-				do{
-					choice=accPTR[i].menu();
-					switch(accPTR[i].getRole()){
-						case "Admin":
-							swtich(choice){
-								case 1: //...
-							}
-							break;
-						case "Teller":
-							swtich(choice){
-								case 1: //...
-							}
-							break;
-						case "Client":
-							swtich(choice){
-								case 1: //...
-							}
-							break;
-						default: break;
-					}
-				sytstem("CLS");
-				}while(flag==1);
-				
-			}
-			
-		}	
-	}while(id!="");
-	//encrypt and write
 	
-	return 0;
+	adminsFile.open("adminList.txt");
+    getline(adminsFile,dataIn);  // Get the frist line from the file, if any.
+    while ( adminsFile ){  // Continue if the line was sucessfully read.
+    	//processLine(dataIn);  // Process the line.
+        getline(adminsFile,dataIn);   // Try to get another line.
 }
+	string test;
+	getline(adminsFile,test);
+	cout<<test<<endl;
+    for (int x = 0; x < dataIn.length(); x++)
+    	{
+        	decryptedData += dataIn[x] ^ key;
+        }
+			decryptedData += space;
+	
+	
+	
+	tellersFile.open("tellerList.txt");
+    getline(tellersFile,dataIn);  // Get the frist line from the file, if any.
+    while ( tellersFile ){  // Continue if the line was sucessfully read.
+    	//processLine(dataIn);  // Process the line.
+        getline(tellersFile,dataIn);   // Try to get another line.
+}
+
+    for (int x = 0; x < dataIn.length(); x++)
+    	{
+        	decryptedData += dataIn[x] ^ key;
+        }
+			decryptedData += space;
+			
+			
+	clientsFile.open("clientList.txt");
+    getline(clientsFile,dataIn);  // Get the frist line from the file, if any.
+    while ( clientsFile ){  // Continue if the line was sucessfully read.
+    	//processLine(dataIn);  // Process the line.
+        getline(clientsFile,dataIn);   // Try to get another line.
+}
+
+    for (int x = 0; x < dataIn.length(); x++)
+    	{
+        	decryptedData += dataIn[x] ^ key;
+        }
+
+                 
+                 
+     
+	
+	
+	
+	return decryptedData;
+	}
 
 
 int saveAndEncrypt()
@@ -118,5 +136,53 @@ int saveAndEncrypt()
 	}
 	clientsFile.close();
 
+	return 0;
+}
+
+
+
+int main(int argc, char** argv) {
+	//variable initialization
+	int choice;
+	int flag;
+	string id, pswd;
+	//decrypt and load
+	
+	//login & menu
+	do{
+		flag=1;
+		cout<<"Please enter account ID and password(to exit program leave blank): ";
+		cin>>id, pswd;
+		for(i=0; i<count;i++){
+			if(accPTR[i].getID()==id && accPTR[i].getPass()==pswd){
+				do{
+					choice=accPTR[i].menu();
+					switch(accPTR[i].getRole()){
+						case "Admin":
+							swtich(choice){
+								case 1: //...
+							}
+							break;
+						case "Teller":
+							swtich(choice){
+								case 1: //...
+							}
+							break;
+						case "Client":
+							swtich(choice){
+								case 1: //...
+							}
+							break;
+						default: break;
+					}
+				sytstem("CLS");
+				}while(flag==1);
+				
+			}
+			
+		}	
+	}while(id!="");
+	//encrypt and write
+	
 	return 0;
 }
