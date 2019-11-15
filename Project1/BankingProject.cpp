ae86a15c6601c37ef6fd980f5b6e148729c881b0
Dev-Yeoj <Dev-Yeoj@users.noreply.github.com>
2019-11-15 00:31:29 -0500

    Added saveAndEncrypt Function
    Added a function that encrypts user data and saves it to three different files depending on which user is being saved. The three files are: adminList.txt, tellerList.txt, and clientList.txt

--- Project1/BankingProject.cpp
+++ Project1/BankingProject.cpp
@@ -9,3 +9,68 @@
 int main(int argc, char** argv) {
 	return 0;
 }
+
+
+
+
+int saveAndEncrypt()
+{
+	char key = "q" //Key to be used with XOR algorithm
+	string INFO =""; //Blank initializer for string: INFO
+	
+	ofstream adminsFile;
+	ofstream tellersFile;
+	ofstream clientsFile;
+	
+	//adminCount  =   The number of admins that were added
+	//adminPTR    =   The pointer array of admins
+	//tellerCount =   The number of tellers that were added
+	//tellerPTR   =   The pointer array of tellers
+	//clientCount =   The number of clients that were added
+	//clientPTR   =   The pointer array of clients
+	
+	adminsFile.open("adminList.txt");
+	//In this for loop the "i" represents which admin in the array of admins
+	for(int i=0, i < adminCount, i++)
+	{
+		
+		//In this for loop the "x" a single character in the string of adminPTR[i]
+		for (int x = 0; x <adminPTR[i].toString().length(); x++)
+    		{
+        		INFO += adminPTR[i].toString()[x] ^ key;
+        	}
+        adminsFile <<INFO<<" "; 
+	}
+	adminsFile.close();
+	
+	
+	
+	
+	tellersFile.open("tellerList.txt");
+	for(int i=0, i < tellerCount, i++)
+	{
+		for (int x = 0; x <tellerPTR[x].toString().length(); x++)
+    		{
+        		INFO += tellerPTR.toString()[x] ^ key;
+        	}
+        tellersFile <<INFO<<" ";
+	}
+	tellersFile.close();
+	
+	
+	
+	
+	clientsFile.open("clientList.txt");
+	for(int i=0, i < clientCount, i++)
+	{
+		for (int x = 0; x <clientPTR[x].toString().length(); x++)
+    		{
+        		INFO += clientPTR.toString()[x] ^ key;
+        	}
+        clientsFile <<INFO<<" ";
+	}
+	clientsFile.close();
+
+	return 0;
+}
+
